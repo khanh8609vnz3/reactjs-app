a9076890fb5dd18301d69fd5ecf270f3ae72d3ce
Khanh <khanh9507@gmail.com>
2019-03-17 11:40:48 +0700

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import LoginPage from "./pages/LoginPage";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import "./scss/app.scss";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/LoginPage";
 class App extends Component {
   textValue = "Day la text";
   render() {
     return (
-      <div className="App">
-        <Header />
-        <LoginPage />
-      </div>
+      <BrowserRouter>
+        <div className="Menu">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/login" component={Login} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
